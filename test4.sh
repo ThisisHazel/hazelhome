0168af50f34dfe93461e6eb3ea05d193b41f6c38
ThisisHazel <106460805+ThisisHazel@users.noreply.github.com>
2022-05-30 23:56:00 +0900

    Update test4.sh

--- test4.sh
+++ test4.sh
@@ -5,3 +5,5 @@ Initialized empty Git repository in /home/work/git_project/.git/
 git config --global use.name "HAZEL"
 git config --global user.email djkimms4880@gmail.com
 echo KNOUHOMEWORK > test.sh
+git add test.sh
+git commit -m "test.sh 1"
