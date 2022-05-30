ec9fc7b0e7be289a43c7a784e9cfe481a1bc616a
ThisisHazel <106460805+ThisisHazel@users.noreply.github.com>
2022-05-30 23:46:26 +0900

    Update test4.sh

--- test4.sh
+++ test4.sh
@@ -1,1 +1,7 @@
-hello github
+mkdir git_project
+cd git_project
+git init
+Initialized empty Git repository in /home/work/git_project/.git/
+git config --global use.name "HAZEL"
+git config --global user.email djkimms4880@gmail.com
+echo KNOUHOMEWORK > test.sh
